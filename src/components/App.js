16540474e539c044b81f5aa9d4076352fc8f6499
KrysDev <webdevkrys@gmail.com>
2023-01-16 20:35:14 +0000

    x

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <input type="number" />
+      xxx
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
+// 1) import
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './components/App';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+
+// 2) reference to ID root and
+// 3) take control of that element
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// 4) create a component
+//      imported
+
+// 5) show the component on the screen
 root.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
